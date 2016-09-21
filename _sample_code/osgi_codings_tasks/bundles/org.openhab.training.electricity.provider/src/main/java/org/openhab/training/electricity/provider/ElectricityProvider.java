44fb82d00ceb26147d3d3d80909a82b8d862e97f
Kiril Atanasov <mail@kisat.eu>
2016-09-21 16:07:59 +0300

    Coding tasks for OSGi, II. Services Sample implementations for the [second part of the Coding tasks for OSGi](http://docs.openhab.org/developers/prerequisites/osgitasks.html#ii-services)
    Signed-off-by: Kiril Atanasov <kiril.atanasov@musala.com> (github: kirilAtan)

---
+++ _sample_code/osgi_codings_tasks/bundles/org.openhab.training.electricity.provider/src/main/java/org/openhab/training/electricity/provider/ElectricityProvider.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2014-2016 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.training.electricity.provider;
+
+/**
+ * An {@link ElectricityProvider} is a representation of electricity provider,
+ * which can provide electricity to consumers.
+ * 
+ * @author Kiril Atanasov - Initial contribution
+ */
+public interface ElectricityProvider {
+
+    /**
+     * Indicates whether an {@link ElectricityProvider} can provide enough energy
+     * to a consumer.
+     * 
+     * @param value
+     *            the energy consumption of the consumer
+     * @return true if the provider can satisfy the consumer's energy
+     *         requirements, false otherwise
+     */
+    boolean provide(int value);
+}
