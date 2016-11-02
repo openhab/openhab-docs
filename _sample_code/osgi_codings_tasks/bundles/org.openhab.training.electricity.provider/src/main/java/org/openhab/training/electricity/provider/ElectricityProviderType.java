958388d6c562c0b7cde7ff7032fb00cbc2d4fc90
Kiril Atanasov <mail@kisat.eu>
2016-11-02 11:37:08 +0200

    Add coding tasks for OSGi, III. Service Tracker (#113)
    Sample implementations of the [third part of the OSGi Coding Tasks](http://docs.openhab.org/developers/prerequisites/osgitasks.html#iii-service-tracker).
    
    Modified org.openhab.training.electricity.provider bundle.
    
    Also-by: Elena Miovska <elena.miovska@musala.com> (github: emiovska)
    Signed-off-by: Kiril Atanasov <kiril.atanasov@musala.com> (github: kirilAtan)

---
+++ _sample_code/osgi_codings_tasks/bundles/org.openhab.training.electricity.provider/src/main/java/org/openhab/training/electricity/provider/ElectricityProviderType.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2014-2016 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.training.electricity.provider;
+
+/**
+ * The type of the {@link ElectricityProvider}.
+ * 
+ * @author Kiril Atanasov - Initial contribution
+ */
+public enum ElectricityProviderType {
+    HOME_NETWORK,
+    BATTERY,
+    SOLAR_SOURCE;
+ }
