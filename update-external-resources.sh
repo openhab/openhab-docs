fcfe50500b103e21c6e79e48d7b037dc8ad20165
Jerome Luckenbach <github@luckenba.ch>
2018-06-14 21:25:24 +0200

    Added google assistant to external ressources.
    Signed-off-by: Jerome Luckenbach <github@luckenba.ch>

--- update-external-resources.sh
+++ update-external-resources.sh
@@ -53,6 +53,7 @@ pull_or_clone_repo "openhabian" "openhab/openhabian.git"
 pull_or_clone_repo "openhab-alexa" "openhab/openhab-alexa.git"
 pull_or_clone_repo "openhab-mycroft" "openhab/openhab-mycroft.git"
 pull_or_clone_repo "openhab-android" "openhab/openhab-android.git"
+pull_or_clone_repo "openhab-google-assistant" "openhab/openhab-google-assistant.git"
 
 echo_process "Updating submodules of the 'openhab-bundles' repo... "
 git -C "$resourcefolder/openhab-bundles" submodule update --recursive --remote --init
