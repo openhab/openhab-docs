5f576edfdccf8741597577fca15963e799753871
Jerome Luckenbach <github@luckenba.ch>
2020-01-04 16:17:50 +0100

    Fix artifactory file handling.
    Signed-off-by: Jerome Luckenbach <github@luckenba.ch>

--- update-external-resources.sh
+++ update-external-resources.sh
@@ -64,9 +64,10 @@ echo_process "Updating submodules of the 'openhab-bundles' repo... "
 git -C "$resourcefolder/openhab-bundles" submodule update --recursive --remote --init
 
 echo_process "Fetching feature.xml file from the snapshot repository..."
-wget -r -l 1 -npdH -A '*.xml' -P "$resourcefolder/jfrog-files" "https://openhab.jfrog.io/openhab/libs-snapshot/org/openhab/distro/openhab-addons/2.5.1-SNAPSHOT/"
-# Copy the lates feature file into the finally used feature.xml
-cp `ls .external-resources/jfrog-files/openhab-addons-2.5.0-*-features.xml | sort | tail -1` .external-resources/jfrog-files/feature.xml
+# Getting all possibly relevant xml files
+wget -r -l 2 -npdH -A '*2.5.*.xml' -P "$resourcefolder/jfrog-files" "https://openhab.jfrog.io/openhab/libs-snapshot/org/openhab/distro/openhab-addons/"
+# Copy the latest feature file into the finally used feature.xml
+cp `ls .external-resources/jfrog-files/openhab-addons-2.5.*-*-features.xml | sort | tail -1` .external-resources/jfrog-files/feature.xml
 
 echo_process "Running Maven Clean Plugin... "
 mvn clean
