e80496ff1e6571687bbade5d4cbed11e167e1a13
Jerome Luckenbach <github@luckenba.ch>
2020-01-04 14:41:05 +0100

    Fix website build and introduce 2.5.x based docs (#1131)
    * Introduced function parameter and renamed openhab-addons repository.
    
    Signed-off-by: Jerome Luckenbach <github@luckenba.ch>
    
    * Remove double quotes
    
    Signed-off-by: Jerome Luckenbach <github@luckenba.ch>
    
    * Clone new repos on specific branch
    
    Signed-off-by: Jerome Luckenbach <github@luckenba.ch>
    
    * Trim branch parameter
    
    Signed-off-by: Jerome Luckenbach <github@luckenba.ch>
    
    * Revert third parameter change.
    
    Signed-off-by: Jerome Luckenbach <github@luckenba.ch>

--- update-external-resources.sh
+++ update-external-resources.sh
@@ -34,33 +34,37 @@ echo_process "Updating the base openhab-docs repo... (skipping)"
 # Parameters: $1=name, $2=GitHub project
 pull_or_clone_repo() {
   if [ -d "$resourcefolder/$1" ]; then
+    # Make sure to get all available branches in case of future script adaptions
+    git -C "$resourcefolder/$1" fetch origin -v
+
     echo_process "Updating the '$1' repo... "
-    git -C "$resourcefolder/$1" checkout master
+    git -C "$resourcefolder/$1" checkout $3
     git -C "$resourcefolder/$1" pull
   else
     echo_process "Cloning the '$1' repo... "
     mkdir "$resourcefolder/$1"
-    git clone "https://github.com/$2" "$resourcefolder/$1"
+    git clone --branch $3 "https://github.com/$2" "$resourcefolder/$1"
   fi
 }
 
-pull_or_clone_repo "smarthome" "eclipse/smarthome.git"
-pull_or_clone_repo "openhab-distro" "openhab/openhab-distro.git"
-pull_or_clone_repo "openhab1-addons" "openhab/openhab1-addons.git"
-pull_or_clone_repo "openhab2-addons" "openhab/openhab2-addons.git"
-pull_or_clone_repo "openhab-bundles" "openhab/openhab-bundles.git"
-pull_or_clone_repo "openhabian" "openhab/openhabian.git"
-pull_or_clone_repo "openhab-alexa" "openhab/openhab-alexa.git"
-pull_or_clone_repo "openhab-mycroft" "openhab/openhab-mycroft.git"
-pull_or_clone_repo "openhab-android" "openhab/openhab-android.git"
-pull_or_clone_repo "openhab-google-assistant" "openhab/openhab-google-assistant.git"
-pull_or_clone_repo "openhab-webui" "openhab/openhab-webui.git"
+# Pull or clone the repo with a specified branch into the given folder
+pull_or_clone_repo "smarthome" "eclipse/smarthome.git" master
+pull_or_clone_repo "openhab-distro" "openhab/openhab-distro.git" 2.5.x
+pull_or_clone_repo "openhab1-addons" "openhab/openhab1-addons.git" master
+pull_or_clone_repo "openhab-addons" "openhab/openhab-addons.git" 2.5.x
+pull_or_clone_repo "openhab-bundles" "openhab/openhab-bundles.git" 2.5.x
+pull_or_clone_repo "openhabian" "openhab/openhabian.git" master
+pull_or_clone_repo "openhab-alexa" "openhab/openhab-alexa.git" master
+pull_or_clone_repo "openhab-mycroft" "openhab/openhab-mycroft.git" master
+pull_or_clone_repo "openhab-android" "openhab/openhab-android.git" master
+pull_or_clone_repo "openhab-google-assistant" "openhab/openhab-google-assistant.git" master
+pull_or_clone_repo "openhab-webui" "openhab/openhab-webui.git" 2.5.x
 
 echo_process "Updating submodules of the 'openhab-bundles' repo... "
 git -C "$resourcefolder/openhab-bundles" submodule update --recursive --remote --init
 
 echo_process "Fetching feature.xml file from the snapshot repository..."
-wget -r -l 1 -npdH -A '*.xml' -P "$resourcefolder/jfrog-files" "https://openhab.jfrog.io/openhab/libs-snapshot/org/openhab/distro/openhab-addons/2.5.0-SNAPSHOT/"
+wget -r -l 1 -npdH -A '*.xml' -P "$resourcefolder/jfrog-files" "https://openhab.jfrog.io/openhab/libs-snapshot/org/openhab/distro/openhab-addons/2.5.1-SNAPSHOT/"
 # Copy the lates feature file into the finally used feature.xml
 cp `ls .external-resources/jfrog-files/openhab-addons-2.5.0-*-features.xml | sort | tail -1` .external-resources/jfrog-files/feature.xml
 
