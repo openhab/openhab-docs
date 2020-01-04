8a58815989efbe2156dbfeca7d16a1017cdff0fd
Jerome Luckenbach <github@luckenba.ch>
2020-01-04 14:26:26 +0100

    Revert third parameter change.
    Signed-off-by: Jerome Luckenbach <github@luckenba.ch>

--- update-external-resources.sh
+++ update-external-resources.sh
@@ -34,8 +34,11 @@ echo_process "Updating the base openhab-docs repo... (skipping)"
 # Parameters: $1=name, $2=GitHub project
 pull_or_clone_repo() {
   if [ -d "$resourcefolder/$1" ]; then
+    # Make sure to get all available branches in case of future script adaptions
+    git -C "$resourcefolder/$1" fetch origin -v
+
     echo_process "Updating the '$1' repo... "
-    git -C "$resourcefolder/$1" checkout $(echo "${3//[$'\t\r\n ']}")
+    git -C "$resourcefolder/$1" checkout $3
     git -C "$resourcefolder/$1" pull
   else
     echo_process "Cloning the '$1' repo... "
@@ -44,6 +47,7 @@ pull_or_clone_repo() {
   fi
 }
 
+# Pull or clone the repo with a specified branch into the given folder
 pull_or_clone_repo "smarthome" "eclipse/smarthome.git" master
 pull_or_clone_repo "openhab-distro" "openhab/openhab-distro.git" 2.5.x
 pull_or_clone_repo "openhab1-addons" "openhab/openhab1-addons.git" master
@@ -60,7 +64,7 @@ echo_process "Updating submodules of the 'openhab-bundles' repo... "
 git -C "$resourcefolder/openhab-bundles" submodule update --recursive --remote --init
 
 echo_process "Fetching feature.xml file from the snapshot repository..."
-wget -r -l 1 -npdH -A '*.xml' -P "$resourcefolder/jfrog-files" "https://openhab.jfrog.io/openhab/libs-snapshot/org/openhab/distro/openhab-addons/2.5.0-SNAPSHOT/"
+wget -r -l 1 -npdH -A '*.xml' -P "$resourcefolder/jfrog-files" "https://openhab.jfrog.io/openhab/libs-snapshot/org/openhab/distro/openhab-addons/2.5.1-SNAPSHOT/"
 # Copy the lates feature file into the finally used feature.xml
 cp `ls .external-resources/jfrog-files/openhab-addons-2.5.0-*-features.xml | sort | tail -1` .external-resources/jfrog-files/feature.xml
 
