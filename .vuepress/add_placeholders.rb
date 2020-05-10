c382002f5543c99b74cbd69a3369788e9a34d184
Yannick Schaus <github@schaus.net>
2020-05-10 20:06:08 +0200

    Fix placeholder pages
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- .vuepress/add_placeholders.rb
+++ .vuepress/add_placeholders.rb
@@ -3,23 +3,25 @@
 def add_placeholder_pages()
     puts ">>> Adding placeholder pages for preview"
     [
-        "docs/concepts",
         "docs/configuration/iconsets/classic",
         "docs/configuration/ui/habmin",
         "docs/configuration/ui/habot",
-        "docs/configuration/ui/basic",
         "docs/configuration/ui/basic",
         "docs/configuration/ui/classic",
         "docs/ecosystem/alexa",
         "docs/ecosystem/google-assistant",
         "docs/ecosystem/ifttt",
-        "docs/ecosystem/ifttt",
         "docs/ecosystem/mycroft",
-        "addons/integrations/homekit"
-    ].each { |dir|
-        puts " -> #{dir}/"
-        FileUtils.mkdir_p(dir)
-        File.open("#{dir}/readme.md", "w+") { |f|
+        "addons/integrations/homekit",
+        "addons/uis/apps/android.md"
+    ].each { |path|
+        puts " -> #{path}"
+        page = path
+        if (!(path =~ /\.md/)) then
+            FileUtils.mkdir_p(path)
+            page = path + "/readme.md"
+        end
+        File.open(page, "w+") { |f|
             f.puts "# This content is unavailable"
             f.puts ""
             f.puts "This content is migrated from another repository, and is not included in this preview."
