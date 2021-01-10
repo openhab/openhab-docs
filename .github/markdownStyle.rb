fb86020a47471b07c64a4371999c2764ec3e146a
Jerome Luckenbach <github@luckenba.ch>
2021-01-10 15:51:57 +0100

    Improved and synced merkdown linting rules.
    Signed-off-by: Jerome Luckenbach <github@luckenba.ch>

--- .github/markdownStyle.rb
+++ .github/markdownStyle.rb
@@ -9,6 +9,9 @@ exclude_rule 'MD009'
 # Allow long line lengths
 exclude_rule 'MD013'
 
+# Allow duplicate headers for different nesting
+rule 'MD024', :allow_different_nesting  => true
+
 # Allow Multiple top level headers in the same document
 exclude_rule 'MD025'
 
